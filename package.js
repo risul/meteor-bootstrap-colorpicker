6eacc4f321bc41af5038073d24e40c0c595ef761
root <risul.islam@regfire.com>
2015-11-11 22:15:30 +0100

    Updated to 2.3.0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'risul:bootstrap-colorpicker',
     summary: 'Bootstrap Colorpicker: http://mjolnic.com/bootstrap-colorpicker/',
-    version: '2.2.0',
+    version: '2.3.0',
     git: 'https://github.com/risul/meteor-bootstrap-colorpicker'
 });
 
 Package.on_use(function (api) {
     api.versionsFrom("METEOR@1.0");
 
-    api.addFiles([
+    api.addAssets([
         "lib/img/alpha-horizontal.png",
         "lib/img/alpha.png",
         "lib/img/hue-horizontal.png",
